7c55c1fb8585d05166520ac77aa8c057045e7266
Carsten Bauer <crstnbr@gmail.com>
2023-02-22 15:08:56 +0100

    basic test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,19 @@
 using MPITape
+using MPI
 using Test
 
-@testset "MPITape.jl" begin
-    # Write your tests here.
+testdir = @__DIR__
+testfiles = sort(filter(startswith("mpitest_"), readdir(testdir)))
+nprocs = clamp(Sys.CPU_THREADS, 2, 5)
+
+atexit(MPITape.cleanup) # rm tape files
+
+@testset "$f" for f in testfiles
+    mpiexec() do mpirun
+        function cmd(n = nprocs)
+            `$mpirun -n $n $(Base.julia_cmd()) --startup-file=no $(joinpath(testdir, f))`
+        end
+        run(cmd())
+        @test true
+    end
 end
