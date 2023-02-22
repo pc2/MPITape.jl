a59b3ba2df8256478767a50527ff442ebb1fe35a
Carsten Bauer <crstnbr@gmail.com>
2023-02-22 08:17:31 +0100

    basic docs

---
+++ docs/make.jl
@@ -0,0 +1,38 @@
+using Documenter
+using DocThemePC2
+using MPITape
+
+const ci = get(ENV, "CI", "") == "true"
+
+@info "Preparing DocThemePC2"
+DocThemePC2.install(@__DIR__)
+
+@info "Generating Documenter.jl site"
+makedocs(;
+         sitename = "MPITape.jl",
+         authors = "Carsten Bauer",
+         modules = [MPITape],
+         checkdocs = :exports,
+         # doctest = ci,
+         pages = [
+             "MPITape" => "index.md",
+             "Examples" => [
+                 "TBD" => "examples/ex_basics.md",
+             ],
+             "References" => [
+                 "API" => "refs/api.md",
+             ],
+         ],
+         # assets = ["assets/custom.css", "assets/custom.js"]
+         repo = "https://github.com/carstenbauer/MPITape.jl/blob/{commit}{path}#{line}",
+         format = Documenter.HTML(; collapselevel = 1))
+
+if ci
+    @info "Deploying documentation to GitHub"
+    deploydocs(;
+               repo = "github.com/carstenbauer/MPITape.jl.git",
+               devbranch = "main",
+               push_preview=true,
+               # target = "site",
+               )
+end
---
+++ docs/src/index.md
@@ -0,0 +1,23 @@
+# MPITape.jl
+
+## Installation
+
+**Note: Only Linux is supported!**
+
+The package is not registered. You can use
+```
+] add https://github.com/carstenbauer/MPITape.jl
+```
+to add the package to your Julia environment.
+
+### Prerequisites
+
+TBD
+
+## Noteworthy Alternatives
+
+TBD
+
+## Acknowledgements
+
+* CI infrastructure is provided by the [Paderborn Center for Parallel Computing (PC²)](https://pc2.uni-paderborn.de/)
